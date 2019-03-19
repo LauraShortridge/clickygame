14878b21d66ca7d53987834acb375c00537568fa
unknown <veggie_munchiesandcrunchies@hotmail.com>
2019-03-19 19:22:46 -0400

    Updates

---
+++ precache-manifest.c19e5fc390be3523c7e5824363a08e5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "be0cb318636c48967842",
+    "url": "/clickygame/static/js/main.47c5a072.chunk.js"
+  },
+  {
+    "revision": "a49af3237dbddbb747fb",
+    "url": "/clickygame/static/js/2.650383ae.chunk.js"
+  },
+  {
+    "revision": "be0cb318636c48967842",
+    "url": "/clickygame/static/css/main.b92e6ab9.chunk.css"
+  },
+  {
+    "revision": "afdbb90dd81661e38bd41f1af9e13d82",
+    "url": "/clickygame/index.html"
+  }
+];
